5696231da9b947055fcab60fb75b0fa823c173ea
DonovanAlexander123 <144191156+DonovanAlexander123@users.noreply.github.com>
2023-10-11 18:30:10 -0600

    Add files via upload

---
+++ p1/p2_1S22.java
@@ -0,0 +1,10 @@
+class P2_1S22 {public static void main(String[] args) {
+	System.out.println("Nombre: Donovan Alexander Zavala Cobos");	
+    System.out.println("Edad: 18 años");
+    System.out.println("Correo: zdonovan907@gmail.com");
+    System.out.println("Aficion: Jugar videojuegos");
+    System.out.println("Color favorito: Verde:)");
+
+}
+}
+ 
