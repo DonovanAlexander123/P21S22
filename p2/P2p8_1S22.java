685df381ff26ef011210e775ea4031b4fa2ead47
DonovanAlexander123 <144191156+DonovanAlexander123@users.noreply.github.com>
2023-10-20 18:54:16 -0600

    Add files via upload

--- p2/P2p8_1S22.java
+++ p2/P2p8_1S22.java
@@ -1,23 +1,21 @@
-import java.util.Scanner;
 public class P2p8_1S22{
 	public static void main(String[] args){
-       Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el primer numero: ");
-        double num1 = scanner.nextDouble();
+	if(args.length == 3){
+    int a,b,r=0;
+    char operacion;
+    operacion = args[1].charAt(0);
+    a = Integer.parseInt(args[0]);
+    b = Integer.parseInt(args[2]);
+    switch(operacion){
+	case'x': r = a*b;break;
+	case'+': r = a+b;break;
+	case'-': r = a-b;break;
+	case'/': r = a/b;break;
+	}
+    System.out.println("El resultado de la operacion fue: "+r);
+    }else{
+		System.out.println("No se puede realizar la operacion");
+	}
 
-        System.out.print("Ingrese el segundo numero: ");
-        double num2 = scanner.nextDouble();
-
-        double suma = num1 + num2;
-        double resta = num1 - num2;
-        double multiplicacion = num1 * num2;
-        double division = num1 / num2;
-
-        System.out.println("Suma: " + num1 + " + " + num2 + " = " + suma);
-        System.out.println("Resta: " + num1 + " - " + num2 + " = " + resta);
-        System.out.println("Multiplicacion: " + num1 + " * " + num2 + " = " + multiplicacion);
-        System.out.println("Division: " + num1 + " / " + num2 + " = " + division);
-
-        scanner.close();
-    }
+	}
 }
