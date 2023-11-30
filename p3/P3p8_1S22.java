08bed9d79a444e5d5ab8c091a66bec5cae191e91
DonovanAlexander123 <144191156+DonovanAlexander123@users.noreply.github.com>
2023-11-29 20:02:14 -0600

    Add files via upload

---
+++ p3/P3p8_1S22.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class P3p8_1S22 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double x;
+
+        System.out.println("Ingrese un número:");
+        x = teclado.nextDouble();
+        
+        double resultado = cuadrado(x) - (1.0 / 5.0);
+
+        resultado += x * x - (1.0 / 5.0) * 3;
+
+        System.out.println("El resultado es: " + resultado);
+    }
+
+    public static double cuadrado(double x) {
+        return x * x;
+     public static int multiplicar(int 3){
+        return x * 3;
+     public static double resta(1/5) {
+        return x * x;
+
+    }
+}
