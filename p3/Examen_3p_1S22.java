f884686598c09ac9dbc14de7b70a88379699f58b
DonovanAlexander123 <144191156+DonovanAlexander123@users.noreply.github.com>
2024-01-09 19:23:28 -0600

    Add files via upload

---
+++ p3/Examen_3p_1S22.java
@@ -0,0 +1,26 @@
+public class Examen_3p_1S22{
+public static void main(String[] args){
+for (int i = 1; i<=100; i++) {
+if (i % 3== 0 && i % 5 == 0){
+System.out.println("blanco y azul");
+}
+if (i % 3== 0){
+System.out.println("blanco");
+}else{
+
+if (i % 5== 0){
+System.out.println("azul");
+}else{
+System.out.println(i);
+
+
+
+}
+}
+}
+}
+}
+
+
+
+
